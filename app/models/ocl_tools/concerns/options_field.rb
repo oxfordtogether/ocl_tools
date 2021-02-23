7dbceeacd7c155801fb3dcc2f7a51e17b196ca4e
Tom Close <tom.close@cantab.net>
2021-02-23 13:41:19 +0000

    Add OptionsField

--- app/models/ocl_tools/concerns/options_field.rb
+++ app/models/ocl_tools/concerns/options_field.rb
@@ -1,30 +1,34 @@
-module OptionsField
-  extend ActiveSupport::Concern
+module OclTools
+  module Concerns
+    module OptionsField
+      extend ActiveSupport::Concern
 
-  module ClassMethods
-    # options_field :a, {b: "B desc", c: "C", d: "D"}, prefix: true
-    #
-    # is equivalent to
-    #
-    # enum a: { b: 'b', c: 'c', d: 'd'}, _prefix: true
-    #
-    # and will also add the methods
-    #
-    # - MyClass.a_options_for_select
-    # - my_object.humanized_a
+      module ClassMethods
+        # options_field :a, {b: "B desc", c: "C", d: "D"}, prefix: true
+        #
+        # is equivalent to
+        #
+        # enum a: { b: 'b', c: 'c', d: 'd'}, _prefix: true
+        #
+        # and will also add the methods
+        #
+        # - MyClass.a_options_for_select
+        # - my_object.humanized_a
 
-    def options_field(name, options, prefix: true)
-      # we're going to lean on rails' enum helper for most of this
-      converted_definitions = { name => options.map { |k, _v| [k, k.to_s] }.to_h }
-      converted_definitions[:_prefix] = prefix if prefix
-      enum converted_definitions
+        def options_field(name, options, prefix: true)
+          # we're going to lean on rails' enum helper for most of this
+          converted_definitions = { name => options.map { |k, _v| [k, k.to_s] }.to_h }
+          converted_definitions[:_prefix] = prefix if prefix
+          enum converted_definitions
 
-      # rails' select will accept arrays like [["Some description", :some_option], ..]
-      define_singleton_method("#{name}_options_for_select") { options.map { |k, v| [v, k] } }
+          # rails' select will accept arrays like [["Some description", :some_option], ..]
+          define_singleton_method("#{name}_options_for_select") { options.map { |k, v| [v, k] } }
 
-      # get the field to always return a symbol
-      define_method(name) { attributes[name.to_s]&.to_sym }
-      define_method("humanized_#{name}") { options[send(name)] }
+          # get the field to always return a symbol
+          define_method(name) { attributes[name.to_s]&.to_sym }
+          define_method("humanized_#{name}") { options[send(name)] }
+        end
+      end
     end
   end
 end
