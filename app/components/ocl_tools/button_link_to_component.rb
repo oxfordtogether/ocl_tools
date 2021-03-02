6fbc1cc38a36fc545eedd1a9efafe27e52213946
Anthony Aimon <anthony.aimon@gmail.com>
2021-03-02 14:51:08 +0000

    add more buttons to the link_to component

--- app/components/ocl_tools/button_link_to_component.rb
+++ app/components/ocl_tools/button_link_to_component.rb
@@ -2,12 +2,13 @@ module OclTools
   class ButtonLinkToComponent < ViewComponent::Base
     delegate :icon, to: :helpers
 
-    def initialize(text, path, icon: nil)
+    def initialize(text, path, type, icon: nil)
       @text = text
       @path = path
+      @type = type
       @icon_name = icon
     end
 
-    attr_reader :text, :path, :icon_name
+    attr_reader :text, :path, :type, :icon_name
   end
 end
