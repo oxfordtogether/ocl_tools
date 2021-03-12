4a64a9788515a5fe38643a6927c242c6204e55e2
Tom Close <tom.close@cantab.net>
2021-03-12 17:10:49 +0000

    introduce style parameter for button_link_to

--- app/components/ocl_tools/button_link_to_component.rb
+++ app/components/ocl_tools/button_link_to_component.rb
@@ -2,13 +2,22 @@ module OclTools
   class ButtonLinkToComponent < ViewComponent::Base
     delegate :icon, to: :helpers
 
-    def initialize(text, path, type, icon: nil)
+    Style = Struct.new(:name, :link_classes, :icon_classes)
+    STYLES = [
+      Style.new("gray-outline", "border-gray-300 text-gray-700 bg-white hover:bg-gray-50", "text-gray-500"),
+      Style.new("primary-outline", "border-transparent text-primary-700 bg-primary-100 hover:bg-primary-200", "text-primary-500"),
+      Style.new("primary-filled", "border-transparent text-white bg-primary-600 hover:bg-primary-700", "text-white")
+    ]
+
+    def initialize(text, path, style: "gray-outline", icon: nil)
       @text = text
       @path = path
-      @type = type
       @icon_name = icon
+
+      @style = STYLES.find {|s| s.name == style} or raise "Unrecognised style: #{style}"
     end
 
-    attr_reader :text, :path, :type, :icon_name
+    attr_reader :text, :path, :style, :icon_name
   end
+
 end
