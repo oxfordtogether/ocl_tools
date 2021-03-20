96be517fb43245b2be1421210c69c5231538fdff
Tom Close <tom.close@cantab.net>
2021-03-20 13:47:22 +0000

    add type to alert_component

--- app/components/ocl_tools/alert_component.rb
+++ app/components/ocl_tools/alert_component.rb
@@ -2,9 +2,28 @@ module OclTools
   class AlertComponent < ViewComponent::Base
     delegate :icon, to: :helpers
 
-    def initialize(title: nil, info: nil)
+    attr_reader :title, :type, :info
+
+    SUPPORTED_TYPES ||= %i[warn info danger].freeze
+
+    def initialize(title: nil, info: nil, type: :warn)
       @title = title
       @info = info
+      raise "Unsupported type: #{type}" unless SUPPORTED_TYPES.include?(type)
+
+      @type = type
+    end
+
+    def warn?
+      type == :warn
+    end
+
+    def info?
+      type == :info
+    end
+
+    def danger?
+      type == :danger
     end
   end
 end
