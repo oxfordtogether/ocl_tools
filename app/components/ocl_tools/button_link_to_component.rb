1af49afdf74bc45213e8ad03363585ad0a1bd93d
Tom Close <tom.close@cantab.net>
2021-03-20 13:51:04 +0000

    allow for options to be passed to button_link_to_component

--- app/components/ocl_tools/button_link_to_component.rb
+++ app/components/ocl_tools/button_link_to_component.rb
@@ -6,18 +6,18 @@ module OclTools
     STYLES = [
       Style.new("gray-outline", "border-gray-300 text-gray-700 bg-white hover:bg-gray-50", "text-gray-500"),
       Style.new("primary-outline", "border-transparent text-primary-700 bg-primary-100 hover:bg-primary-200", "text-primary-500"),
-      Style.new("primary-filled", "border-transparent text-white bg-primary-600 hover:bg-primary-700", "text-white")
+      Style.new("primary-filled", "border-transparent text-white bg-primary-600 hover:bg-primary-700", "text-white"),
     ]
 
-    def initialize(text, path, style: "gray-outline", icon: nil)
+    def initialize(text, path, options: {}, style: "gray-outline", icon: nil)
       @text = text
       @path = path
       @icon_name = icon
+      @options = options
 
-      @style = STYLES.find {|s| s.name == style} or raise "Unrecognised style: #{style}"
+      @style = STYLES.find { |s| s.name == style } or raise "Unrecognised style: #{style}"
     end
 
-    attr_reader :text, :path, :style, :icon_name
+    attr_reader :text, :path, :style, :icon_name, :options
   end
-
 end
