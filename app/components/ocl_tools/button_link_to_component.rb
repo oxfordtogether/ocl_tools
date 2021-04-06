506867748a4db8794a85fa10c3d78d1db6e53ba9
VickyClose <vicky.r.close@gmail.com>
2021-04-06 14:56:12 +0100

    Button sizes

--- app/components/ocl_tools/button_link_to_component.rb
+++ app/components/ocl_tools/button_link_to_component.rb
@@ -11,8 +11,9 @@ module OclTools
 
     Size = Struct.new(:name, :link_classes, :icon_classes)
     SIZES = [
-      Size.new("small", "px-2 py-1", "-ml-0.5 mr-1 h-4 w-4"),
-      Size.new("normal", "px-4 py-2", "-ml-1 mr-2 h-5 w-5"),
+      Size.new("very_small", "px-2 py-0.5 text-xs", "-ml-0.5 mr-1 h-4 w-4"),
+      Size.new("small", "px-2 py-1 text-sm", "-ml-0.5 mr-1 h-4 w-4"),
+      Size.new("normal", "px-4 py-2 text-sm", "-ml-1 mr-2 h-5 w-5"),
     ]
 
     def initialize(text, path, options: {}, style: "gray-outline", size: "normal", icon: nil)
