a7f6233f17acc33aaa29048acad5c44cff96d7ff
VickyClose <vicky.r.close@gmail.com>
2021-04-13 20:35:09 +0100

    Index view by status generator

---
+++ lib/generators/ocl_tools/index_view_by_status/index_view_by_status_generator.rb
@@ -0,0 +1,40 @@
+module OclTools
+  class IndexViewByStatusGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path("templates", __dir__)
+    include Rails::Generators::ResourceHelpers
+
+    class_option :section, type: :string, desc: "Name of section, e.g. people"
+    class_option :namespace, type: :string, desc: "Namespace, e.g. admin"
+
+    def generate
+      @section = options["section"]
+      @namespace = options["namespace"]
+      @index_head_path = "#{@namespace}/#{@section}/index_head"
+
+      @statuses = model_name.constantize.send("status_options_for_select").map(&:reverse).to_h
+
+      template "index.html.erb", File.join("app/views", regular_class_path, plural_name, "index.html.erb")
+
+      template "controller.rb", File.join("app/controllers", regular_class_path, "#{plural_name}_controller.rb")
+
+      route %(
+      resources :#{plural_name}, only: %i[index], path: '#{plural_name}/:status', constraints: { status: /all|#{@statuses.keys.join("|")}/ }
+      ), namespace: regular_class_path
+    end
+
+    private
+
+    def index_path(namespace, name)
+      "#{namespace}_#{name}_path"
+    end
+
+    def model_name
+      file_name.camelize
+    end
+
+    def base_controller_class_name
+      (controller_class_path + ["base"]).map!(&:camelize).join("::")
+    end
+  end
+end
+
