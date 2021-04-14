e67a6cdb8cd076dfd9a9bf35c6a0940f6655da63
Tom Close <tom.close@cantab.net>
2021-04-14 16:53:16 +0100

    add day_short format

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,4 +1,5 @@
 day_long = ->(date) { date.strftime("%a #{date.day.ordinalize} %b, %Y") }
+day_short = ->(date) { date.strftime("%a #{date.day.ordinalize} %b") }
 relative = lambda do |date|
   if date.today? && date.to_date != date
     # we don't want to display 00:00 when it's a date or a time that was converted from a date
@@ -14,5 +15,6 @@ end
 am_pm = "%l:%M%P"
 
 Time::DATE_FORMATS[:day_long] = Date::DATE_FORMATS[:day_long] = day_long
+Time::DATE_FORMATS[:day_short] = Date::DATE_FORMATS[:day_short] = day_short
 Time::DATE_FORMATS[:relative] = Date::DATE_FORMATS[:relative] = relative
 Time::DATE_FORMATS[:am_pm] = am_pm
