8721264a95b8d242bc6d4852a703e5d121c39229
VickyClose <vicky.r.close@gmail.com>
2021-04-14 13:23:40 +0100

    Update search generator

--- lib/generators/ocl_tools/search/search_generator.rb
+++ lib/generators/ocl_tools/search/search_generator.rb
@@ -3,12 +3,8 @@ module OclTools
     source_root File.expand_path("templates", __dir__)
 
     def create
-      copy_file "search.html.erb", "app/views/pages/search.html.erb"
-
-      copy_file "search_spec.rb", "spec/interactors/search_spec.rb"
-
       copy_file "search_reflex.rb", "app/reflexes/search_reflex.rb"
-      copy_file "search_controller.js", "app/javascript/controllers/search_controller.js"
+      copy_file "application_reflex.rb", "app/reflexes/application_reflex.rb"
 
       copy_file "search_cache.rb", "app/interactors/search_cache.rb"
       copy_file "search_cache_refresh.rb", "app/workers/search_cache_refresh.rb"
--- lib/generators/ocl_tools/search/templates/search_cache.rb
+++ lib/generators/ocl_tools/search/templates/search_cache.rb
@@ -19,31 +19,28 @@ class SearchCache
   end
 
   def self.refresh
-    value = Person.all.sort_by(&:name).map { |p| [p.id, to_searchable_string(p)] }.to_json
+    value = People.all.sort_by(&:name).map { |p| [p.id, p.class.name, to_searchable_string(p)] }.to_json
     redis.set(KEY, value)
     value
   end
 
   def self.get_people_ids(query, limit: nil)
     people = JSON.parse(redis.get(KEY) || refresh)
-    people_ids = people.filter { |_id, details| /#{normalize(query)}/i =~ details }.map { |id, _details| id }
+    people_ids = people.filter { |_id, class_name, details| /#{normalize(query)}/i =~ details }.map { |id, class_name, _details| [id, class_name] }
     SearchResult.new(limit ? people_ids[0...limit] : people_ids, people_ids.length)
-  end
-
-  # TODO: limit/total_count is a bit broken, as it's a limit/total_count on the people not the roles
-  def self.get_roles(query, limit: nil)
-    people_ids = get_people_ids(query, limit: limit)
-    roles = Person.where(id: people_ids.to_a).with_roles.flat_map(&:roles)
-    SearchResult.new(roles, people_ids.total_count)
   end
 
   def self.get_people(query, limit: nil)
     result = get_people_ids(query, limit: limit)
-    SearchResult.new(Person.where(id: result.to_a).all, result.total_count)
+
+    person_results = result.filter { |id, class_name| class_name == "Person" }.map { |id, class_name| id }
+
+    SearchResult.new(
+        Person.where(id: person_results.to_a ).all,
+        result.total_count)
   end
 
   def self.to_searchable_string(person)
-    # configure, e.g. if you want to be able to search by DoB as well
     normalize(person.name)
   end
 
