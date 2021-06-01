42656c852e7f03ee35b3772283bb48c7480cc791
VickyClose <vicky.r.close@gmail.com>
2021-06-01 13:22:49 +0100

    Time select helper and update header component

---
+++ lib/ocl_tools/spec_helpers/time_select.rb
@@ -0,0 +1,11 @@
+module OclTools
+  module TimeSelect
+    def time_select(hour, min, from:)
+      container = find(:label, text: from).find(:xpath, "../..")
+
+      id_prefix = container.find("label")[:for]
+      select hour, from: "#{id_prefix}_4i"
+      select min, from: "#{id_prefix}_5i"
+    end
+  end
+end
