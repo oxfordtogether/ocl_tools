10e5c9697ffdfc23215d2df23c2bf1b081bd1d08
Tom Close <tom.close@cantab.net>
2021-10-05 22:31:15 +0100

    handle classes like p-1.5 correctly

--- postcss-list-classes.js
+++ postcss-list-classes.js
@@ -14,15 +14,21 @@ module.exports = (opts = {}) => {
           // .a.b.c
           // .a:b
           rule.selectors.forEach((selector) =>
-            selector.match(/\.[^.\s]*/g)?.forEach((rawClass) => {
+            // .a.b .p-1/.5 -> ['.a', '.b', '.p-1.5']
+            // dot-not-preceeded-by-slash then multiple slash-dot-OR-not-space-or-dot
+            selector.match(/(?<!\/)\.(\\\.|[^\s\.])*/g)?.forEach((rawClass) => {
               // want to transform
               // .text-sm -> text-sm
               // .md/:text-sm -> md:text-sm
               // .focus/:text-red-400:focus -> focus:text-red-400
               // .hover/:text-red-400:hover -> hover:text-red-400
+              // but also
+              // p-1/.5 -> p-1.5
               let formattedClass = rawClass
                 .replace(/(.*?)([^\\]):.*/, "$1$2") // strip off final :xxxx not preceded by \:
-                .replace(/[\.\\]/g, ""); // get rid of . and \: -> :
+                .replace(/^\./, "") // strip . from the beginning
+                .replace(/\\:/g, ":") // \: -> :
+                .replace(/\\\./g, "."); // \. -> .
               classes.add(formattedClass);
             })
           );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: ["./app/**/*.html.erb", "./app/components/*", "./app/helpers/*"],
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/components/ocl_tools/*",
+    "./app/helpers/*",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
