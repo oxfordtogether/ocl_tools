e113a607d971088ee33e31cad64f86c89d478d6d
Tom Close <tom.close@cantab.net>
2021-11-30 22:30:25 +0000

    allow for town or city in Address

--- app/types/ocl_tools/address.rb
+++ app/types/ocl_tools/address.rb
@@ -1,6 +1,6 @@
 module OclTools
   class Address
-    ATTRS = %i[line_1 line_2 line_3 town postcode]
+    ATTRS = %i[line_1 line_2 line_3 town city postcode] # some apps have town, some have city 🤦‍♂️
     attr_accessor(*ATTRS)
 
     def initialize(attrs = {})
@@ -8,7 +8,7 @@ module OclTools
     end
 
     def self.from(obj, prefix: :address)
-      new(ATTRS.map { |a| [a, obj.send("#{prefix}_#{a}")] }.to_h)
+      new(ATTRS.map { |a| [a, obj.try("#{prefix}_#{a}")] }.to_h)
     end
   end
 end
