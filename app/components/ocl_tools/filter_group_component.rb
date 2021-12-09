8efca5c5ac4d1604bbce51ef08ce2cde0f90e177
Tom Close <tom.close@cantab.net>
2021-12-09 16:12:07 +0000

    nest FilterGroupComponent within OclTools namespace

--- app/components/ocl_tools/filter_group_component.rb
+++ app/components/ocl_tools/filter_group_component.rb
@@ -1,27 +1,29 @@
-class FilterGroupComponent < ViewComponent::Base
-  attr_reader :params, :key, :options, :include_all, :size
+module OclTools
+  class FilterGroupComponent < ViewComponent::Base
+    attr_reader :params, :key, :options, :include_all, :size
 
-  def initialize(params, key:, options:, include_all: true, size: 'very_small')
-    @params = params
-    @key = key
-    @options = options
-    @include_all = include_all
-    @size = SIZES.find { |s| s.name == size } or raise(SizeError, size)
-  end
-
-  Size = Struct.new(:name, :link_classes)
-  SIZES = [
-    Size.new('very_small', 'px-2 py-0.5 text-xs'),
-    Size.new('small', 'px-2 py-1 text-sm'),
-    Size.new('normal', 'px-4 py-2 text-base')
-  ]
-  class SizeError < ArgumentError
-    def initialize(size)
-      @size = size
+    def initialize(params, key:, options:, include_all: true, size: 'very_small')
+      @params = params
+      @key = key
+      @options = options
+      @include_all = include_all
+      @size = SIZES.find { |s| s.name == size } or raise(SizeError, size)
     end
 
-    def message
-      "Unrecognised size: #{@size}. Allowed styles: #{SIZES.map(&:name).join(', ')}"
+    Size = Struct.new(:name, :link_classes)
+    SIZES = [
+      Size.new('very_small', 'px-2 py-0.5 text-xs'),
+      Size.new('small', 'px-2 py-1 text-sm'),
+      Size.new('normal', 'px-4 py-2 text-base')
+    ]
+    class SizeError < ArgumentError
+      def initialize(size)
+        @size = size
+      end
+
+      def message
+        "Unrecognised size: #{@size}. Allowed styles: #{SIZES.map(&:name).join(', ')}"
+      end
     end
   end
 end
