32e8bcf3ccb24d16b73eb080dd195138d76dc49f
Tom Close <tom.close@cantab.net>
2022-01-10 17:15:45 +0000

    get Process::Attributes to work

--- app/lib/ocl_tools/process/attributes.rb
+++ app/lib/ocl_tools/process/attributes.rb
@@ -1,15 +1,23 @@
 module OclTools
   module Process
     module Attributes
+      class AttributesError < ArgumentError
+        def initialize(type)
+          @type = type
+        end
+    
+        def message
+          "Type #{@type.inspect} is not supported. Choose one of: #{Attributes::ALLOWED_TYPES.map(&:inspect).join(', ')}"
+        end
+      end
+
       extend ActiveSupport::Concern
+      include ActiveModel::Model # for validations?
+      include OclTools::Actions::OptionsField
+      include ActiveRecord::AttributeAssignment
 
       ALLOWED_TYPES = %i[string integer date]
 
-      included do
-        include ActiveModel::Model # for validations?
-        include OclTools::Actions::OptionsField
-        include ActiveRecord::AttributeAssignment
-      end
 
       class_methods do
         def attribute_names
@@ -19,30 +27,40 @@ module OclTools
         def attribute(name, type: :string)
           raise AttributesError.new(type) if !ALLOWED_TYPES.include?(type)
 
-          attr_reader name
+          attr_accessor name
 
           attribute_names.push(name)
 
-          define_method("#{name}=") do |value|
+          define_method("extract_#{name}") do |params|
+            params ||= {}
             case type
             when :string
-              instance_variable_set("@#{name}", value)
+              self.send("#{name}=", params[name]) if params.has_key?(name)
             when :integer
-              instance_variable_set("@#{name}", value&.to_i)
+              self.send("#{name}=", params[name]&.to_i) if params.has_key?(name)
             when :date
-              date_or_string = value
-              # ActiveRecord::AttributeAssignment will pass through a hash of the form:
-              # "{3=>8, 2=>9, 1=>2022}"
-              # There must be a better way than this...
-              if date_or_string.is_a?(String)
-                m = /\{3=>(\d+), 2=>(\d+), 1=>(\d+)\}/.match(date_or_string)
-                raise "Error parsing date: #{date_or_string}" unless m
-      
-                date = Date.new(m[3].to_i, m[2].to_i, m[1].to_i)
-              else
-                date = date_or_string
+              # note: we don't do any checks on type here
+              return self.send("#{name}=", params[name]) if params.has_key?(name)
+
+              year_key = "#{name}(1i)"
+              month_key = "#{name}(2i)"
+              day_key = "#{name}(3i)"
+
+              if [year_key, month_key, day_key].all? {|k| params.has_key?(k)}
+                # note: nil.to_i == "".to_i == 0
+                year = params[year_key]&.to_i
+                month = params[month_key]&.to_i
+                day = params[day_key]&.to_i
+
+                begin
+                  date = Date.new(year, month, day)
+                rescue Date::Error
+                  # note: if part of date is missing, this will clear others
+                  date = nil
+                end
+
+                self.send("#{name}=", date)
               end
-              instance_variable_set("@#{name}", date)
             end
           end
         end
@@ -53,19 +71,9 @@ module OclTools
       end
 
       def assign_attributes(params = {})
-        params.try(:permit!)
-        attribute_params = (params || {}).slice(self.class.attribute_names)
-        super(attribute_params)
-      end
-    end
-
-    class AttributesError < ArgumentError
-      def initialize(type)
-        @type = type
-      end
-  
-      def message
-        "Type #{@type.inspect} is not supported. Choose one of: #{Attributes::ALLOWED_TYPES.map(&:inspect).join(', ')}"
+        self.class.attribute_names.each do |a|
+          send("extract_#{a}", params) 
+        end
       end
     end
   end
