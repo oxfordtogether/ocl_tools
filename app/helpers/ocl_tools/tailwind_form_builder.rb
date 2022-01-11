1285821c828177f72077cecd1212d3d37ece1b0b
Tom Close <tom.close@cantab.net>
2022-01-11 11:04:57 +0000

    add unstyled_button to tailwind form builder

--- app/helpers/ocl_tools/tailwind_form_builder.rb
+++ app/helpers/ocl_tools/tailwind_form_builder.rb
@@ -216,6 +216,15 @@ module OclTools
       super(label, options, &block)
     end
 
+    def unstyled_button(label = nil,  name: nil, value: nil, **options, &block)
+      if value
+        options[:value] = value
+        options[:name] = "#{object_name}[#{name || 'commit'}]"
+      end
+
+      orig_button(label, options, &block)
+    end
+
     def cancel(action, label = "Cancel", compact: false)
       @template.link_to(label, action, class: "inline-flex justify-center bg-white #{compact ? 'py-1 px-2' : 'py-2 px-4'} border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500")
     end
