682637ec59bbf92ecbabd019381dbbe90bba9299
Tom Close <tom.close@cantab.net>
2022-01-13 02:14:45 +0000

    option to skip padding on pagination component

--- app/components/ocl_tools/pagination_component.rb
+++ app/components/ocl_tools/pagination_component.rb
@@ -3,9 +3,12 @@ module OclTools
     include Pagy::Frontend
     delegate :icon, to: :helpers
 
-    def initialize(pagy:, things:)
+    attr_reader :no_padding
+
+    def initialize(pagy:, things:, no_padding: false)
       @pagy = pagy
       @things = things
+      @no_padding = no_padding
     end
   end
 end
