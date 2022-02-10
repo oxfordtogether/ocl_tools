ac0c5fd705b8f4164e02503fb50e60f2ded8d71e
Tom Close <tom.close@cantab.net>
2022-02-10 08:45:36 +0000

    ProcessWrapperModal component

---
+++ app/components/ocl_tools/process_wrapper_modal_component.rb
@@ -0,0 +1,34 @@
+module OclTools
+  class ProcessWrapperModalComponent < ViewComponent::Base
+    delegate :icon, :tailwind_form_with, to: :helpers
+    attr_reader :process
+
+    WIDTHS ||= %i[wide narrow].freeze
+
+    def initialize(process, scope: nil, width: nil, component: nil)
+      raise "Unknown width: #{width.inspect}. Allowed values: #{WIDTHS.map(&:inspect)}" if width && ! WIDTHS.include?(width)
+      @width = width || :narrow
+      @process = process
+      @scope = scope
+      @component = component
+      super
+    end
+
+    def process_component
+      @component || "#{process.class.to_s.split('::Process').first}::Component".constantize
+    end
+
+    def scope
+      @scope || process.class.to_s.split('::Process').first.underscore
+    end
+
+    def current_path
+      request.fullpath
+    end
+
+    def narrow?
+      @width == :narrow
+    end
+  end
+end
+
