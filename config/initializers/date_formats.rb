ff6c098891af1c9671ceabbd59ea732e8b3261f0
Tom Close <tom.close@cantab.net>
2022-03-23 10:09:46 +0000

    add ord_short and ord_long date formats

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,10 +1,13 @@
 day_long = ->(date) { date.strftime("%a #{date.day.ordinalize} %b, %Y") }
 day_short = ->(date) { date.strftime("%a #{date.day.ordinalize} %b") }
+ord_long = ->(date) { date.strftime("#{date.day.ordinalize} %b, %Y") }
+ord_short = ->(date) { date.strftime("#{date.day.ordinalize} %b") }
+
 relative = lambda do |date|
   if date.today? && date.to_date != date
     # we don't want to display 00:00 when it's a date or a time that was converted from a date
     # date.to_date != date is true if and only if the time part is non-zero
-    date.strftime("%H:%M")
+    date.strftime('%H:%M')
   elsif date.year == Date.today.year
     date.strftime("#{date.day.ordinalize} %b")
   else
@@ -12,7 +15,7 @@ relative = lambda do |date|
   end
 end
 
-am_pm = "%l:%M%P"
+am_pm = '%l:%M%P'
 
 days_since = lambda do |date|
   if date.today?
@@ -26,8 +29,9 @@ end
 
 Time::DATE_FORMATS[:day_long] = Date::DATE_FORMATS[:day_long] = day_long
 Time::DATE_FORMATS[:day_short] = Date::DATE_FORMATS[:day_short] = day_short
+Time::DATE_FORMATS[:ord_long] = Date::DATE_FORMATS[:ord_long] = ord_long
+Time::DATE_FORMATS[:ord_short] = Date::DATE_FORMATS[:ord_short] = ord_short
 Time::DATE_FORMATS[:relative] = Date::DATE_FORMATS[:relative] = relative
 Time::DATE_FORMATS[:days_since] = Date::DATE_FORMATS[:days_since] = days_since
 
 Time::DATE_FORMATS[:am_pm] = am_pm
-
