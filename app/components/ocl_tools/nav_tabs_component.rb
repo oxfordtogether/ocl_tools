8095f3b42bcccefee24e270cac5dce9d1757e1e2
VickyClose <vicky.r.close@gmail.com>
2022-05-19 15:21:01 +0100

    Handle query params

--- app/components/ocl_tools/nav_tabs_component.rb
+++ app/components/ocl_tools/nav_tabs_component.rb
@@ -7,19 +7,21 @@ module OclTools
       yield self
     end
 
-    def link(name, path, partial_match: false)
-      @links << NavTabsLink.new(name, path, partial_match)
+    def link(name, path, partial_match: false, ignore_query_params: true)
+      @links << NavTabsLink.new(name, path, partial_match, ignore_query_params)
     end
 
     def links
       # view components only allow you to call controller helpers like current_page? at render time
       @links.map do |link|
-        is_current = link.partial_match ? request.fullpath.start_with?(link.path) : current_page?(link.path)
+        path = link.ignore_query_params ? URI(link.path).path : link.path
+
+        is_current = link.partial_match ? request.fullpath.start_with?(path) : current_page?(path)
         NavTabsLinkWithActive.new(link.name, link.path, is_current)
       end
     end
   end
 
-  NavTabsLink = Struct.new(:name, :path, :partial_match)
+  NavTabsLink = Struct.new(:name, :path, :partial_match, :ignore_query_params)
   NavTabsLinkWithActive = Struct.new(:name, :path, :active?)
 end
