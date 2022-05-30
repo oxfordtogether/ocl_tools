3b0ce3960bde207be52c1afe82152e3b89e04ada
Tom Close <tom.close@cantab.net>
2022-05-30 14:40:38 +0100

    add description_radio_group_select spec helper

--- lib/ocl_tools/spec_helpers/radio_group_select.rb
+++ lib/ocl_tools/spec_helpers/radio_group_select.rb
@@ -3,5 +3,9 @@ module OclTools
     def radio_group_select(label, value)
       find('label', text: label.to_s).find(:xpath, '../../../..').choose(value.to_s)
     end
+
+    def description_radio_group_select(label, value)
+      find('legend', text: label.to_s).find(:xpath, '..').find('label', text: value.to_s).find(:xpath, '../..').choose
+    end
   end
 end
